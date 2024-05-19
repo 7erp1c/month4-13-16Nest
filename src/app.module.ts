6871703d1640bee8dec62fac8aae3ee8f8710284
7erp1c <ul_tray@bk.ru>
2024-05-19 17:22:31 +0300

    New project Nest

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.1:27017', {
+      dbName: 'DataBase',
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
